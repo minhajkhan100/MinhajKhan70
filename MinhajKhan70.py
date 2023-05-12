3333a7c92ba7dd13626976e783df185009899d00
minhajkhan100 <133322303+minhajkhan100@users.noreply.github.com>
2023-05-12 15:57:23 +0600

    Create MinhajKhan70.py

---
+++ MinhajKhan70.py
@@ -0,0 +1,25 @@
+# jisan7
+
+           🔥Hello Friend🔥
+
+            🍁Old Id Hack🍁
+
+ 👿 apt update && apt upgrade
+ 
+ 👿 apt install python
+
+ 👿 pip install mechanize lolcat
+ 
+ 👿 futurespip install requests bs4
+ 
+ 👿 pip install futures
+ 
+ 👿 apt install git
+
+ 👿 git clone https://github.com/Jisan70/jisan7
+
+ 👿 cd jisan7
+ 
+ 👿 ls
+ 
+ 👿 python jisan-ahamed.py
